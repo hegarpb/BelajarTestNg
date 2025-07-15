59fa46ad4b938957733af5fdefdc9cdce53237a2
hegarpb <hegaregayz@gmail.com>
2025-07-15 19:42:03 +0700

    add atribut enable

--- src/test/java/com/juaracoding/btestng/CalculatorTest.java
+++ src/test/java/com/juaracoding/btestng/CalculatorTest.java
@@ -1,26 +1,6 @@
 package com.juaracoding.btestng;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
 
 public class CalculatorTest {
-@Test
-public void addTest() {
-    int a = 10;
-    int b = 2;
-    int expected = 12;
 
-    int actual = Calculator.add(a, b);
-    Assert.assertEquals(actual, expected);
-}
-
-@Test
-public void substractTest() {
-    int a = 10;
-    int b = 2;
-    int expected = 8;
-    int actual = Calculator.substract(a, b);
-
-    Assert.assertEquals(expected, actual);
-}
 }
--- src/test/java/com/juaracoding/btestng/SignInTest.java
+++ src/test/java/com/juaracoding/btestng/SignInTest.java
@@ -1,20 +1,22 @@
 package com.juaracoding.btestng;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SignInTest {
-@Test
-public void tc003() {
-    String username = "johansebastian";
-    String password = "123qwe";
 
-    SignIn signIn = new SignIn();
+  @Test(priority = 1)
+  public void signinTest() {
+    System.out.println("Method SignInTest.signinTest dijalankan");
+  }
 
-    signIn.setUsername(username);
-    signIn.setPassword(password);
-    boolean actual = signIn.onSignIn();
+  @Test(priority = 2)
+  public void dashboardTest() {
+    System.out.println("Method SignInTest.dashboardTest dijalankan");
+  }
 
-    Assert.assertTrue(actual);
-}
+  @Test(priority = 3)
+  public void profileTest() {
+    System.out.println("Method SignInTest.profileTest dijalankan");
+  }
+
 }
